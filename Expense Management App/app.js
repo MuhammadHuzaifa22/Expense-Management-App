34123c3c8a19d8f6a1e05e99731ec31661a98588
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-07-27 09:55:24 +0500

    40% work completed

--- Expense Management App/app.js
+++ Expense Management App/app.js
@@ -1,89 +1,88 @@
-// import {onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-// import { auth } from "/config.js"
+const listSection = document.querySelector('.list-section');
+const form = document.getElementById('expense-from');
+const expenseType = document.getElementById('expense-type');
+const expenseAmount = document.getElementById('expense');
+const ulExpenseType = document.getElementById('ul-expense-type');
+const ulExpenseDate = document.getElementById('ul-expense-date');
+const ulExpenseAmount = document.getElementById('ul-expense-amount');
+let arr = [];
 
 
-// onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//       const uid = user.uid;
-//       console.log("🚀 ~ onAuthStateChanged ~ uid:", uid)
-//     } else {
-//       window.location = 'register.html';
-//     }
-//   });
-  
-  
-  const listSection = document.querySelector('.list-section');
-  const form = document.getElementById('expense-from');
-  const expenseType = document.getElementById('expense-type');
-  const expenseAmount = document.getElementById('expense');
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  if ((expenseType.value === '' || expenseType.value === null) && expenseAmount.value === '' || expenseAmount.value === null) {
+    alert(`Please fill input fields`);
+  } else if ((expenseType.value === '' || expenseType.value === null) && (expenseAmount.value !== '' || expenseAmount.value !== null)) {
+    alert(`Please enter *expense type*`);
+  } else if ((expenseAmount.value === '' || expenseAmount.value === null) && (expenseType.value !== '' || expenseType.value !== null)) {
+    alert(`Please enter *expense amount*`);
+  } else {
 
 
-  
-  let arr = [];
-  
-  form.addEventListener('submit', event =>{
-    event.preventDefault();
-    if((expenseType.value === '' || expenseType.value === null) && expenseAmount.value === '' || expenseAmount.value === null){
-      alert(`Please fill input fields`);
-    }else if((expenseType.value === '' || expenseType.value === null) && (expenseAmount.value !== '' || expenseAmount.value !== null)){
-      alert(`Please enter *expense type*`);
-    }else if((expenseAmount.value === '' || expenseAmount.value === null) && (expenseType.value !== '' || expenseType.value !== null)){
-      alert(`Please enter *expense amount*`);
-    }
-    
-      let now = new Date();
-      let formattedDateTime = now.toLocaleString();
-  
-      let obj = {
+    let now = new Date();
+    let formattedDateTime = now.toLocaleString();
+
+    let obj = {
       Type: `${expenseType.value}`,
-      Amount : `${expenseAmount.value}`,
-      DateAndTime :`${formattedDateTime}`
+      Amount: `${expenseAmount.value}`,
+      DateAndTime: `${formattedDateTime}`
     }
+
 
     listSection.style.display = 'block';
     listSection.style.transition = '0.6s ease in';
     arr.push(obj);
     console.log(arr)
-
     console.log(expenseType.value);
     console.log(expenseAmount.value)
     console.log("🚀 ~ arr:", arr);
-
-    renderList(arr);  
-    
-    let totalAmount = 0;
-    let totalAmountArray = [];
-    totalAmountArray.push(obj.Amount);
-    console.log("🚀 ~ renderList ~ totalAmount:", totalAmountArray);
-    
-  })
-
-const ulExpenseType = document.getElementById('ul-expense-type');
-const ulExpenseDate = document.getElementById('ul-expense-date');
-const ulExpenseAmount = document.getElementById('ul-expense-amount');
-
-
-function renderList(arr){
-    ulExpenseDate.innerHTML = '';
-    ulExpenseType.innerHTML = '';
-    ulExpenseAmount.innerHTML = '';
-
-  for(let i = 0; i <= arr.length;i++){
-    ulExpenseType.innerHTML += `<li>${arr[i].Type} <button id="editt">Edit</button></li>`
-    ulExpenseAmount.innerHTML += `<li>${arr[i].Amount} <button onclick="editLiExpenseAmount(${i})">Edit</button></li>`
-    ulExpenseDate.innerHTML += `<li>${arr[i].DateAndTime}</li>`    
-    const Edit = document.getElementById('editt');
-    Edit.onclick = () => deleteLiExpenseType(i)
+    renderList();
   }
-}
-  
-function deleteLiExpenseType(i){
+
+})
+
+
+function renderList() {
   ulExpenseType.innerHTML = '';
- const Delete = 'sds'
- const arrSplice = arr.splice(i,1);
- console.log(arrSplice);
- for(let j = 0; j <= arr.length;j++){
-  ulExpenseType.innerHTML = `${arr[j]}`
- }
- 
+  ulExpenseAmount.innerHTML = '';
+  ulExpenseDate.innerHTML = '';
+  arr.map((item, index) => {
+    ulExpenseType.innerHTML += `<li>${item.Type} <button class="editBut" id="dataContainer" onclick="editExpense(${index})">Edit <i class="fa-solid fa-pen-to-square"></i></button></li>`;
+    ulExpenseAmount.innerHTML += `<li>${item.Amount}  <button class="edit-button-app-js" onclick="editAmount(${index})">Edit <i class="fa-solid fa-pen-to-square"></i></button></li>`;
+    ulExpenseDate.innerHTML += `<li>${item.DateAndTime}</li>`;
+  });
+  
 }
+
+function editExpense(index) {
+  const updateExpenseType = prompt('Enter new expense type');
+  
+  let updatedExpense = {
+    Type: updateExpenseType,
+    Amount: arr[index].Amount,
+    DateAndTime: new Date().toLocaleString()
+  }
+  arr.splice(index, 1, updatedExpense)
+  renderList();
+}
+
+function editAmount(index) {
+  const updatedExpenseAmount = prompt(`Enter new expense amount`);
+  let updatedExpense = {
+    Type: arr[index].Type,
+    Amount: updatedExpenseAmount,
+    DateAndTime: new Date().toLocaleString()
+  }
+
+  arr.splice(index, 1, updatedExpense)
+  renderList();
+}
+
+
+const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+const daysDate = new Date();
+const dateNumber = daysDate.getDay();
+
+const Num = days.indexOf(dateNumber);
+console.log("🚀 ~ Num:", Num)
